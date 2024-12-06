e9f7c01c71a58fda1b20df58467ed072e14cd514
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-06 16:57:57 +0200

    feat: reading puzzle input and printing prettily

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::{fs::File, io::BufReader};
+
+use aoc_2024_day1::puzzle_input::PuzzleInput;
+
+fn main() -> std::io::Result<()> {
     println!("solving part 1...");
+
+    let input_string = include_str!("../../input/numbers.txt");
+    let input = PuzzleInput::from(input_string);
+
+    println!("{}", input);
+
+    Ok(())
 }
--- src/puzzle_input.rs
+++ src/puzzle_input.rs
@@ -1,6 +1,42 @@
+use std::fmt::Display;
+
 pub struct Pair {
     left: i32,
     right: i32,
 }
 
 pub struct PuzzleInput(Vec<Pair>);
+
+impl Display for PuzzleInput {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for pair in &self.0 {
+            writeln!(f, "({}, {})", pair.left, pair.right)?;
+        }
+        Ok(())
+    }
+}
+
+impl From::<&str> for PuzzleInput {
+    fn from(value: &str) -> Self {
+        PuzzleInput(value
+            .trim()
+            .lines()
+            .map(|line| {
+                let split = line.split_whitespace();
+                let tokens: Result<Vec<_>, _> = split
+                    .map(|token| token.parse::<i32>())
+                    .collect();
+
+                // TODO
+                let tokens = tokens.unwrap();
+                assert_eq!(tokens.len(), 2);
+
+                Pair {
+                    left: tokens[0],
+                    right: tokens[1],
+                }
+            })
+            .collect()
+        )
+    }
+}
