ad8c3b362c4c200924b11d1be019b51f9109ec58
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-06 18:33:01 +0200

    feat: displaying solver on part 2

--- src/bin/part2/main.rs
+++ src/bin/part2/main.rs
@@ -1,3 +1,16 @@
+use aoc_2024_day1::puzzle_input::PuzzleInput;
+use puzzle_solver::PuzzleSolver;
+
+mod puzzle_solver;
+
 fn main() {
     println!("solving part 2...");
+
+    let input_string = include_str!("../../../input/example.txt");
+
+    let input = PuzzleInput::from(input_string);
+    println!("{}", input);
+
+    let solver = PuzzleSolver::from(input);
+    println!("{}", solver);
 }
---
+++ src/bin/part2/puzzle_solver.rs
@@ -0,0 +1,40 @@
+use std::fmt::Display;
+
+use aoc_2024_day1::puzzle_input::PuzzleInput;
+
+pub struct PuzzleSolver {
+    lefts: Vec<i32>,
+    rights_freq: [i32; 100_000],
+}
+
+impl Display for PuzzleSolver {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "lefts: [")?;
+        for i in 0..self.lefts.len() - 1 {
+            write!(f, "{}, ", self.lefts[i])?;
+        }
+        writeln!(f, "{}]", self.lefts[self.lefts.len() - 1])?;
+
+        writeln!(f, "rights frequency: {{")?;
+        for i in 0..self.rights_freq.len() {
+            if self.rights_freq[i] != 0 { writeln!(f, "  {}: {} times", i, self.rights_freq[i])?; }
+        }
+        writeln!(f, "}}")?;
+
+        Ok(())
+    }
+}
+
+impl From::<PuzzleInput> for PuzzleSolver {
+    fn from(value: PuzzleInput) -> Self {
+        let (lefts, rights) = value.split();
+        let mut rights_freq = [0; 100_000];
+
+        for i in 0..rights.len() {
+            let index = rights[i] as usize;
+            rights_freq[index] += 1;
+        }
+
+        Self { lefts, rights_freq }
+    }
+}
