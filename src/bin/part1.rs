18480db60835ef3105378516d22af7e8e2484d02
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-06 17:00:13 +0200

    chore: remove io return type

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,12 +1,10 @@
 use aoc_2024_day1::puzzle_input::PuzzleInput;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     println!("solving part 1...");
 
     let input_string = include_str!("../../input/numbers.txt");
     let input = PuzzleInput::from(input_string);
 
     println!("{}", input);
-
-    Ok(())
 }
