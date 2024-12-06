c523b75e89a3c8052b015e087eb19d66b16d40e1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-06 17:23:41 +0200

    printing stuff to console

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -3,8 +3,11 @@ use aoc_2024_day1::puzzle_input::PuzzleInput;
 fn main() {
     println!("solving part 1...");
 
-    let input_string = include_str!("../../input/numbers.txt");
-    let input = PuzzleInput::from(input_string);
+    let input_string = include_str!("../../input/example.txt");
 
+    let input = PuzzleInput::from(input_string);
     println!("{}", input);
+
+    let split = input.split();
+    println!("{:?}", split);
 }
